de9906a88478d55c920a59c7d879837a49394d7d
Sonosus <67963676+Sonosus@users.noreply.github.com>
2022-04-01 19:19:13 +0100

    Clean up a bit

--- fetchuuid.py
+++ fetchuuid.py
@@ -1,23 +1,29 @@
-import urllib.request json #import required libs
-with urllib.request.urlopen('https://raw.githubusercontent.com/boylejack/kosmodulargrid/main/public/data/makers.json') as f: # fetch remote json file
-    makers_raw = f.read().decode('utf-8') # decode file
-    maker_dict = {"name":"id"} # create empty placeholder dictionary
+import urllib.request 
+import json
 
-makers_count = len(json.loads(makers_raw)) # count entries in loaded json
-for i in range(0,makers_count): # loop x times where x is number of entries
+makers_url = 'https://raw.githubusercontent.com/boylejack/kosmodulargrid/main/public/data/makers.json'
+with urllib.request.urlopen(makers_url) as file:
+    makers_raw = file.read().decode('utf-8') # decode file
+    
+makers_json = json.loads(makers_raw)
+
+num_makers = len(makers_json) # count entries in loaded json
+maker_dict = {}
+for i in range(0,num_makers): # loop x times where x is number of entries
     makers = json.loads(makers_raw)[i]  # load maker from that entry
     #print(type(makers)) #debug: print data type - should be dict
-    ids = makers['id'] # get maker uuid
-    name = makers['name'] # get maker name
-    print(ids," ",name) # print name and id
-    maker_dict[name] = ids # add name and id to dictionary
-print(maker_dict) # debug: print dictionary of all makers
+    id = makers['id'] 
+    name = makers['name']
+    print(id," ",name)
+    maker_dict[name] = id # add name and id to dictionary
 
-def checkname(): #function checkname()
-    queryname = input("Enter name of maker: ") # get name of maker to lookup
+    #print(maker_dict) # debug: print dictionary of all makers
+
+def checkname():
+    queryname = input("Enter name of maker: ")
     if queryname in maker_dict: # check if it's in the list of known makers
         print("OK!")
-        return queryname # return wanted maker
+        return queryname
     else: # if it's not in the list of known makers
         print("Maker name not found, please try again") 
         checkname() # repeat until a known maker is entered
